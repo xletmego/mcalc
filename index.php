c45a8bc660f5006a9b83343af35a6a7bf546b1aa
Dmitry Litvinov <dmitry.litvinov@kub3.ru>
2023-08-28 11:55:30 +0300

    test vba

--- index.php
+++ index.php
@@ -6,6 +6,4 @@ if(empty($_REQUEST)){
 $param1 = $_REQUEST['param1'] ?? 1;
 $param2 = $_REQUEST['param2'] ?? 2;
 
-echo json_encode([
-    'result' => intval($param1) * intval($param2),
-]);
+echo intval($param1) * intval($param2);
